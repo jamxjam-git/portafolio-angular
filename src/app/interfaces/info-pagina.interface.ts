b5adeced290705658975fb8c4566eb1cea32fd41
Jose Angel Mateo <jamxjam@gmail.com>
2020-07-25 10:39:42 +0200

    fin seccion 4

---
+++ src/app/interfaces/info-pagina.interface.ts
@@ -0,0 +1,15 @@
+import { mainModule } from 'process';
+
+
+
+export interface InfoPagina {
+  titulo?: string;
+  email?: string;
+  nombre_corto?: string;
+  pagina_autor?: string;
+  facebook?: string;
+  twitter?: string;
+  instagram?: string;
+  tumblr?: string;
+  equipo_trabajo?: any[];
+}
