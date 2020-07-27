42d12b5b66587a1c540512c9bbade3ffc630311b
Jose Angel Mateo <jamxjam@gmail.com>
2020-07-27 21:05:18 +0200

    Fin seccion 6

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,9 +10,17 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
+  equipo: any[] = [];
+
+
   constructor( private http: HttpClient) {
-    //leer el archivo JSON
-    this.http.get('assets/data/data-pagina.json')
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo () {
+      //leer el archivo JSON
+      this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) =>  {
 
         this.cargada = true;
@@ -20,5 +28,19 @@ export class InfoPaginaService {
         console.log(resp);
         console.log(resp['twitter']);
       })
-   }
+
+  }
+
+  private cargarEquipo() {
+    //leer el archivo JSON
+    this.http.get('https://angular-html-f0bfe.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) =>  {
+
+      this.cargada = true;
+      this.equipo = resp;
+      console.log(resp);
+ 
+    })
+
+}
 }
---
+++ src/app/services/productos.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Producto } from '../interfaces/producto.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductosService {
+
+  cargando = true;
+  productos: Producto[] = [];
+
+  constructor(private http: HttpClient) {
+    this.cargarProductos();
+   }
+
+  cargarProductos() {
+
+    this.http.get("https://angular-html-f0bfe.firebaseio.com/productos_idx.json").subscribe( (resp: Producto[]) => {
+      console.log(resp);
+      this.productos = resp;
+
+      //Este timeout es solo para que se vea el efecto Cargando...
+      setTimeout(() => {
+        this.cargando = false;
+      }, 2000);
+
+    }
+    )
+
+  }
+}
